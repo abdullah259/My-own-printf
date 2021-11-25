c59f49f6e586a4a668baf74e577a3ca24da3de9f
abdullah259 <76406414+abdullah259@users.noreply.github.com>
2021-11-25 18:48:49 +0400

    Add files via upload

--- onlyMakefile/ft_putchar.c
+++ onlyMakefile/ft_putchar.c
@@ -1,11 +1,6 @@
 #include "printf.h"
 
-int     ft_putchar(char c)
+void     ft_putchar(char c)
 {
-    int k;
-
-    k = 0;
     write(1,&c,1);
-    k++;
-    return (k);
 }
--- onlyMakefile/printf.h
+++ onlyMakefile/printf.h
@@ -6,13 +6,13 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-void ft_hex(unsigned int h, char t);
+void 	ft_hex(unsigned int h, char t);
 int     ft_printf(const char *format, ...);
 void    ft_putstr(char *s);
-int     ft_putchar(char c);
-void ft_foru(unsigned int n);
-void ft_putnbr(int n);
-void ft_hexp(unsigned int y);
+void     ft_putchar(char c);
+void 	ft_foru(unsigned int n);
+void 	ft_putnbr(int n);
+void 	ft_hexp(unsigned long long int y);
 #endif
 
 
